bb2277120955223ef2eaa7802756a171d58da3e2
张凯悦 <546854479@qq.com>
2023-05-08 09:34:35 +0800

    del service and controller base,add response_code

--- src/config.php
+++ src/config.php
@@ -6,7 +6,7 @@
  */
 
 return [
-    'stub_path'     => false,
+    'stub_path'     => false,       //模板路径 复制src/stubs/ 下的模板并修改为自己的模板
 
     'model' => [
         'delete_filed' => 'delete_time',
@@ -16,6 +16,13 @@ return [
     ],
     'service' => [
         'admin_event' => true,
+    ],
+    'response_code' => [
+        'success' => 1000,
+        'validate_error' => 1001,
+        'not_login' => 1002,
+        'method_error' => 1003,
+        //自由拓展
     ]
 
 ];
